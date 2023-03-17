67ee8ee9843deea7822dfd834f862df4d15cfd14
chetan314 <chetannandanwar08@gmail.com>
2023-03-17 15:29:20 +0530

    predict pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,21 +49,21 @@ class ModelTrainer:
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
                 },
                 "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                  
-                    # 'max_features':['sqrt','log2',None],
+                    'max_features':['sqrt','log2',None],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     'learning_rate':[.1,.01,.05,.001],
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
+                    'criterion':['squared_error', 'friedman_mse'],
+                    'max_features':['sqrt','log2'],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
@@ -78,7 +78,7 @@ class ModelTrainer:
                 },
                 "AdaBoost Regressor":{
                     'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
+                    'loss':['linear','square','exponential'],
                     'n_estimators': [8,16,32,64,128,256]
                 }
                 
--- src/utils.py
+++ src/utils.py
@@ -5,9 +5,9 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -21,21 +21,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param  ):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para = param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para, cv = 4 , n_jobs = -1 , verbose = True )
+            gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
 
-            #model.fit(X_train, y_train)  # Train model
-
             model.set_params(**gs.best_params_)
-            model.fit(X_train, y_train)  # Train model
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -48,6 +48,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param  ):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
